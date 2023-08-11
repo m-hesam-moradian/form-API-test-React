87b3098c5142f5567efdf03e14133608229e1451
hesam <m.hesam.moradian@gmail.com>
2023-08-11 13:53:24 +0330

    finish

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -2,62 +2,63 @@ import React, { useState } from "react";
 import "./Form.css";
 
 export default function Form() {
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [email, setemail] = useState("");
 
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
+  const registerHandler = (event) => {
+    event.preventDefault();
+    let userInfo = {
+      firstName,
+      lastName,
+      email,
+    };
+    fetch("https://test-37988-default-rtdb.firebaseio.com/userInfo.json", {
+      method: "post",
+      body: JSON.stringify(userInfo),
+    }).then((response) => {
+      if (response.status == 200) {
+        alert("login succes");
+      }
+    });
+  };
+  return (
+    <div className="form-container">
+      <form
+        className="register-form"
+        autoComplete="off"
+        onSubmit={registerHandler}
+      >
+        <input
+          onChange={(event) => setfirstName(event.target.value)}
+          id="first-name"
+          className="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />
+        <input
+          onChange={(event) => setlastName(event.target.value)}
+          id="last-name"
+          className="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
 
-    const registerHandler = event => {
-        event.preventDefault()
+        <input
+          onChange={(event) => setemail(event.target.value)}
+          id="email"
+          className="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
 
-        let userInfo = {
-            firstName,
-            lastName,
-            email
-        }
-
-        fetch('https://sabzlearn-3f278-default-rtdb.firebaseio.com/users.json', {
-            method: 'POST',
-            body: JSON.stringify(userInfo)
-        }).then(response => console.log(response))
-    }
-
-    return (
-        <div className="form-container">
-            <form className="register-form" autoComplete="off" onSubmit={registerHandler}>
-                <input
-                    id="first-name"
-                    value={firstName}
-                    onChange={(event) => setFirstName(event.target.value)}
-                    className="form-field"
-                    type="text"
-                    placeholder="First Name"
-                    name="firstName"
-                />
-                <input
-                    id="last-name"
-                    value={lastName}
-                    onChange={(event) => setLastName(event.target.value)}
-                    className="form-field"
-                    type="text"
-                    placeholder="Last Name"
-                    name="lastName"
-                />
-
-                <input
-                    id="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    className="form-field"
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                />
-                <button className="form-field" type="submit">
-                    Register
-                </button>
-            </form>
-        </div>
-
-    )
+        <button className="form-field" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
